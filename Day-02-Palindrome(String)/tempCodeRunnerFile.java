08604396089ca5d02fed3c5a6ebc85b803a91aac
Saniya-SB <saniya75555@gmail.com>
2026-03-10 23:34:19 +0530

    Day 02 Palindrome String Java

--- Day-02-Palindrome(String)/tempCodeRunnerFile.java
+++
@@ -1,5 +0,0 @@
- if (str==reverse) {
-        System.out.println("A string that you have entered is Palindrome");
-        } else {
-        System.out.println("A string that you have entered is not Palindrome");
-        }
