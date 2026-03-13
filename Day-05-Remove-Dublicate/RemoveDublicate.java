6898d1b8197a1c3e6c6a9087d38b63e9786a9a14
Saniya-SB <saniya75555@gmail.com>
2026-03-13 23:55:37 +0530

    Day 5 Remove dublicate

---
+++ Day-05-Remove-Dublicate/RemoveDublicate.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class RemoveDublicate {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the length of the array");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("Enter a element in index:" + i);
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        int i = 0;
+        for (int j = 1; j < n; j++) {
+            if (arr[i] != arr[j]) {
+                arr[i + 1] = arr[j];
+                i++;
+            }
+
+        }
+
+        System.out.println("Array after removing dublicate elements");
+        for (int j = 0; j < n; j++) {
+            System.out.print(arr[j] + " ");
+        }
+
+    }
+}
