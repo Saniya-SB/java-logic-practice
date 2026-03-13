26b4d146820fa85c790df329202d6c3d7cc7011f
Saniya-SB <saniya75555@gmail.com>
2026-03-14 00:06:36 +0530

    Adding comments in code to understand better

--- Day-05-Remove-Dublicate/RemoveDublicate.java
+++ Day-05-Remove-Dublicate/RemoveDublicate.java
@@ -8,18 +8,22 @@ public class RemoveDublicate {
         System.out.println("Enter the length of the array");
         int n = sc.nextInt();
 
+        // Declaring the lenght of the array
         int[] arr = new int[n];
-
+        // Taking user input in array's index
         for (int i = 0; i < n; i++) {
             System.out.println("Enter a element in index:" + i);
             arr[i] = sc.nextInt();
         }
-
+        // Sorting an array
         Arrays.sort(arr);
+
+        System.out.println("Array after sorting:");
         for (int i = 0; i < n; i++) {
             System.out.print(arr[i] + " ");
         }
         System.out.println();
+        // Code for Removing Dublicate
         int i = 0;
         for (int j = 1; j < n; j++) {
             if (arr[i] != arr[j]) {
@@ -29,7 +33,7 @@ public class RemoveDublicate {
 
         }
 
-        System.out.println("Array after removing dublicate elements");
+        System.out.println("Array after removing dublicate elements:");
         for (int j = 0; j <= i; j++) {
             System.out.print(arr[j] + " ");
         }
