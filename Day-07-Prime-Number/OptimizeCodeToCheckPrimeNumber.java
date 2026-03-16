2b1d3c8dbb67e3bd386f8b31f882dc4fe29ff511
Saniya-SB <saniya75555@gmail.com>
2026-03-16 23:29:29 +0530

    Add comments to make understand the code

--- Day-07-Prime-Number/OptimizeCodeToCheckPrimeNumber.java
+++ Day-07-Prime-Number/OptimizeCodeToCheckPrimeNumber.java
@@ -6,18 +6,22 @@ public class OptimizeCodeToCheckPrimeNumber {
         System.out.println("Enter a number");
         int num = sc.nextInt();
 
+        // if isprime remain true till the end of the code then it will be prime number
         boolean isprime = true;
+
+        // if number is less then or equal to 1 are not prime number
         if (num <= 1) {
             isprime = false;
         }
 
+        // check divisibility only upto sqrt(num) because factors repeats after that
         for (int i = 2; i < Math.sqrt(num); i++) {
             if (num % i == 0) {
                 isprime = false;
                 break;
             }
         }
-
+        // If the number is divisible by i then it has more than two factors ~ not prime
         if (isprime) {
             System.out.println(num + " is a prime number");
         } else {
